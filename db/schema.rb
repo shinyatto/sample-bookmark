ea4d5388ac79997531d9c4709b264272a996931a
shinyatto <technac@gmail.com>
2015-01-01 19:51:10 +0900

    ユーザ追加 migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150101085408) do
+ActiveRecord::Schema.define(version: 20150101104457) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.text     "url",        null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider",   null: false
+    t.string   "uid",        null: false
+    t.string   "nickname",   null: false
+    t.string   "image_url",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "users", ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
 
 end
