4f24e36ce49f19d08ad257d678ad905b6c120e74
shinyatto <technac@gmail.com>
2015-01-01 21:37:21 +0900

    ログインユーザ プロフィール画像表示

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
     session[:user_id] = user.id
+    session[:user_img] = user
     redirect_to root_path, notice: 'ログインしました'
   end
 
