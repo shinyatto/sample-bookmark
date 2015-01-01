c03af86d3d20028c2285b3b19e98e3711c531beb
shinyatto <technac@gmail.com>
2015-01-01 17:59:49 +0900

    add to not null for bookmark and migrate

--- db/migrate/20150101085408_create_bookmarks.rb
+++ db/migrate/20150101085408_create_bookmarks.rb
@@ -1,9 +1,9 @@
 class CreateBookmarks < ActiveRecord::Migration
   def change
     create_table :bookmarks do |t|
-      t.text :url
-      t.text :created_at
-      t.text :updated_at
+      t.text :url,        null: false
+      t.text :created_at, null: false
+      t.text :updated_at, null: false
 
       t.timestamps null: false
     end
