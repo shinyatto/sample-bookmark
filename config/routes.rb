e932ebd4dc6b52a283c127bd38dfa29ef9308409
shinyatto <technac@gmail.com>
2015-01-02 21:39:40 +0900

    ユーザ別ページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get '/logout' => 'sessions#destroy', as: :logout
 
   resources :bookmarks, only: %i{new create show edit update destroy}
+  resources :users, only: %i{show}
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
