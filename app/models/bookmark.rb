87875ec54a84153fb2ae01e4658d3cb574e00473
shinyatto <technac@gmail.com>
2015-01-02 20:32:20 +0900

    コメントカラム 追加 migrate

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ActiveRecord::Base
   belongs_to :owner, class_name: 'User'
   validates :url, length: { maximum: 2000 }, presence: true
+  validates :comment, length: { maximum: 2000 }
   validates :created_at, presence: true
   validates :updated_at, presence: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150102083716) do
+ActiveRecord::Schema.define(version: 20150102112917) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.text     "url",        null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "owner_id"
+    t.text     "comment"
   end
 
   add_index "bookmarks", ["owner_id"], name: "index_bookmarks_on_owner_id"
