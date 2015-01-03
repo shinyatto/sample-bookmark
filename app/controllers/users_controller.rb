5616bf391c070b4f723ed12e9ae3c2b5fcbcbd66
shinyatto <technac@gmail.com>
2015-01-03 17:42:41 +0900

    ハッシュ引数の書き方修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by(:nickname => params[:nickname])
+    @user = User.find_by(nickname: params[:nickname])
   end
 end
